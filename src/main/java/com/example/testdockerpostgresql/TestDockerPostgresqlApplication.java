9ced1632924d8582a670ca2033a1e91921aab54d
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2023-01-19 19:00:29 -0400

    first commit

---
+++ src/main/java/com/example/testdockerpostgresql/TestDockerPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdockerpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDockerPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDockerPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testdockerpostgresql/TestDockerPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdockerpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDockerPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
